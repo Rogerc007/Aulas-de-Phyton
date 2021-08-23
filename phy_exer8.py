65d7898ed43a4ea7a892994f4107c19e36ddf460
Roger <roger.cardozo.lima@hotmail.com>
2021-08-23 11:10:03 -0300

    Operações aritiméticas.

---
+++ phy_exer8.py
@@ -0,0 +1,7 @@
+#Exercício 08 do estudo dirigido (cálculo de Celsius)
+
+feh = float(input("Digite a temperatura em Ferenheit: "))
+
+cel = 5 / 9 * (feh - 32)
+
+print("A temperatura em celsius é:", round(cel, 2))
