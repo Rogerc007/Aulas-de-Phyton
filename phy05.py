531b8c3e09cfb3eb6f8d0744a07d1dc880a4460d
Roger <80080194+Rogerc007@users.noreply.github.com>
2021-08-23 10:39:31 -0300

    Add files via upload

---
+++ phy05.py
@@ -0,0 +1,20 @@
+#Programa para cálculos de duas variáveis.
+
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro número: "))
+
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+potencia = n1 ** n2
+divreal = n1 / n2
+divint = n1 // n2
+resto = n1 % n2
+
+print("A soma é", soma)
+print("A subtracao é", subtracao)
+print("A multiplicação é", multiplicacao)
+print("A potênciação é", potencia)
+print("A divisão real é", divreal)
+print("A divisão inteira é", divint)
+print("O resto da divisão é", resto)
